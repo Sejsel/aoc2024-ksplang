eed07e0e947ec636b3910b60ad510a04dde1f1bd
Jiří Sejkora <sejsel@sejsel.cz>
2024-12-01 15:37:22 +0100

    Port push

--- gen/src/main/kotlin/cz/sejsel/ksplang/std/Push.kt
+++ gen/src/main/kotlin/cz/sejsel/ksplang/std/Push.kt
@@ -1,6 +1,7 @@
 package cz.sejsel.ksplang.std
 
 import cz.sejsel.ksplang.dsl.core.SimpleFunction
+import kotlin.math.abs
 
 fun SimpleFunction.push(n: Long) {
     // TODO: Port "short pushes"
@@ -23,30 +24,28 @@ fun SimpleFunction.push(n: Long) {
             inc()
         }
     } else if (n > 0) {
-        // We could just repeat inc() n times, but that is not viable for big numbers.
-        // Instead, we build it out of powers of two and sum those up
-        /* Original python code:
-        result = []
+        val bitLength = Long.SIZE_BITS - n.countLeadingZeroBits()
+        var numsToAdd = 0
 
-        nums = 0
-        for i in range(n.bit_length()):
-            if n & (1 << i):
-                result.append(push(1))
-                if i == 1:
-                    # We can duplicate the 1 by using m or CS
-                    result.append(m)
-                else:
-                    result.append(push(i))
-                result.append(bitshift)
-                nums += 1
+        for (i in 0 until bitLength) {
+            if (n and (1L shl i) != 0L) {
+                push(1)
+                if (i == 1) {
+                    // We can duplicate the 1 by using m or CS
+                    m()
+                } else {
+                    push(i.toLong())
+                }
+                bitshift()
+                numsToAdd += 1
+            }
+        }
 
-        for _ in range(nums - 1):
-            result.append(_add)
-
-         */
-
-        TODO()
+        repeat(numsToAdd - 1) {
+            add()
+        }
     } else {
-        TODO()
+        push(abs(n))
+        negate()
     }
 }
--- gen/src/test/kotlin/cz/sejsel/ksplang/std/Push.kt
+++ gen/src/test/kotlin/cz/sejsel/ksplang/std/Push.kt
@@ -7,17 +7,34 @@ import cz.sejsel.ksplang.KsplangRunner
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
 import io.kotest.matchers.collections.shouldContainExactly
+import kotlin.random.Random
 
 class PushTests : FunSpec({
     val runner = KsplangRunner()
     val builder = KsplangBuilder()
 
-    context("push(0) onto single value should add 0") {
-        withData(VALUES_PER_DIGIT_SUM) {
+    for (i in 0L..16L) {
+        context("push($i) onto single value should add $i") {
+            withData(VALUES_PER_DIGIT_SUM) {
+                val program = builder.build(function {
+                    push(i)
+                })
+                runner.run(program, listOf(it)) shouldContainExactly listOf(it, i)
+            }
+        }
+    }
+
+    val randomValueCount = 500
+    val random = Random(42)
+    val values = (0..<randomValueCount).map { random.nextLong() }
+    val stackTops = (0..<randomValueCount).map { random.nextLong() }
+
+    context("random tests") {
+        withData(values.zip(stackTops)) { (n, stackTop) ->
             val program = builder.build(function {
-                push(0)
+                push(n)
             })
-            runner.run(program, listOf(it)) shouldContainExactly listOf(it, 0)
+            runner.run(program, listOf(stackTop)) shouldContainExactly listOf(stackTop, n)
         }
     }
 })
