b2dc1a1aab20b5fea5994f90470b1ef73a35991c
Jiří Sejkora <sejsel@sejsel.cz>
2025-08-17 23:05:44 +0200

    Add property tests for instructions

---
+++ wasm2ksplang/src/test/kotlin/cz/sejsel/ksplang/wasm/property/I32Tests.kt
@@ -0,0 +1,73 @@
+package cz.sejsel.ksplang.wasm.property
+
+import com.dylibso.chicory.wasm.types.ValType
+import cz.sejsel.ksplang.DefaultKsplangRunner
+import cz.sejsel.ksplang.builder.KsplangBuilder
+import cz.sejsel.ksplang.dsl.core.buildComplexFunction
+import cz.sejsel.ksplang.wasm.bitsToLong
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+import io.kotest.property.checkAll
+import cz.sejsel.ksplang.wasm.WasmFunctionScope.Companion.initialize as initializeScope
+
+class I32Tests : FunSpec({
+    val runner = DefaultKsplangRunner()
+    val builder = KsplangBuilder()
+
+    context("i32Add") {
+        val function = buildComplexFunction {
+            val scope = initializeScope(listOf(ValType.I32, ValType.I32), listOf(), emptyList())
+            with(scope) {
+                getLocal(0)
+                getLocal(1)
+                i32Add()
+            }
+        }
+        val ksplang = builder.build(function)
+
+        test("i32add(a, b) should equal to (a + b) - uint") {
+            checkAll<UInt, UInt> { a, b ->
+                val input = listOf(a.toLong(), b.toLong())
+                runner.run(ksplang, input) shouldBe input + (a + b).bitsToLong()
+            }
+        }
+
+        test("i32add(a, b) should equal to (a + b) - int") {
+            checkAll<Int, Int> { a, b ->
+                val input = listOf(a.toLong(), b.toLong())
+                // toLong does sign extension, we do not want that
+                val expected = (a + b).bitsToLong()
+                runner.run(ksplang, input) shouldBe input + expected
+            }
+        }
+    }
+
+    context("i32Sub") {
+        val function = buildComplexFunction {
+            val scope = initializeScope(listOf(ValType.I32, ValType.I32), listOf(), emptyList())
+            with(scope) {
+                getLocal(0)
+                getLocal(1)
+                i32Sub()
+            }
+        }
+        val ksplang = builder.build(function)
+
+        test("i32add(a, b) should equal to (a - b) - uint") {
+            checkAll<UInt, UInt> { a, b ->
+                val input = listOf(a.toLong(), b.toLong())
+                runner.run(ksplang, input) shouldBe input + (a - b).bitsToLong()
+            }
+        }
+
+        test("i32add(a, b) should equal to (a - b) - int") {
+            checkAll<Int, Int> { a, b ->
+                val input = listOf(a.toLong(), b.toLong())
+                // toLong does sign extension, we do not want that
+                val expected = (a - b).bitsToLong()
+                runner.run(ksplang, input) shouldBe input + expected
+            }
+        }
+    }
+})
