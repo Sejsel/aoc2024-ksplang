0d19d032f6940aba09813b929d0e61dbe8b0339f
Jiří Sejkora <sejsel@sejsel.cz>
2025-10-24 00:28:02 +0200

    Add f32/f64.store

--- wasm2ksplang/src/test/kotlin/cz/sejsel/ksplang/wasm/chicorycomparison/instructions/StoreTests.kt
+++ wasm2ksplang/src/test/kotlin/cz/sejsel/ksplang/wasm/chicorycomparison/instructions/StoreTests.kt
@@ -123,4 +123,98 @@ class StoreTests : FunSpec({
             }
         }
     }
+
+    context("f32.store") {
+        val wat = $$"""
+        (module 
+            (memory (export "mem") 1 1)
+            ;; 8 bytes: 
+            (data (i32.const 0x0000) "\F0\0D\BE\EF\F0\CA\CC\1A")
+            (func $load (export "store") (param $index i32) (param $value f32)
+                local.get $index
+                local.get $value
+                f32.store
+            )
+        )""".trimIndent()
+
+        val store = Store()
+        val module = instantiateModuleFromWat(translator, wat, "test", store)
+        val program = buildSingleModuleProgram(module) {
+            val store = getExportedFunction("store") as ProgramFunction2To0
+
+            body {
+                call(store)
+            }
+        }
+        val ksplang = builder.buildAnnotated(program).toRunnableProgram()
+
+        test("chicory gets the same result") {
+            checkAll<Long, Int>(Exhaustive.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 5648), Arb.int()) { i, value ->
+                val input = listOf(i, value.bitsToLong())
+
+                val referenceStore = Store()
+                val func = referenceStore.instantiate("mod", module.module.chicoryModule).export("store")!!
+                func.apply(*input.toLongArray())
+                val chicoryMemory = referenceStore.toImportValues().memories().single()
+                val expectedBytes = chicoryMemory.memory().readBytes(0, 65536)
+
+                val result = runner.run(ksplang, input)
+                // We expect layout to be as follows (no fun table, no globals):
+                // 0 input_len mem_size mem_max_size [memory] [input]
+                val memory = result.subList(4, 4 + 65536)
+                memory.forEachIndexed { index, value ->
+                    val expected = expectedBytes[index].toUByte().toLong()
+                    value shouldBe expected
+                    value shouldBeLessThan 256L // just a sanity check really
+                }
+            }
+        }
+    }
+
+    context("f64.store") {
+        val wat = $$"""
+        (module 
+            (memory (export "mem") 1 1)
+            ;; 8 bytes: 
+            (data (i32.const 0x0000) "\F0\0D\BE\EF\F0\CA\CC\1A")
+            (func $load (export "store") (param $index i32) (param $value f64)
+                local.get $index
+                local.get $value
+                f64.store
+            )
+        )""".trimIndent()
+
+        val store = Store()
+        val module = instantiateModuleFromWat(translator, wat, "test", store)
+        val program = buildSingleModuleProgram(module) {
+            val store = getExportedFunction("store") as ProgramFunction2To0
+
+            body {
+                call(store)
+            }
+        }
+        val ksplang = builder.buildAnnotated(program).toRunnableProgram()
+
+        test("chicory gets the same result") {
+            checkAll<Long, Long>(Exhaustive.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 5648), Arb.long()) { i, value ->
+                val input = listOf(i, value)
+
+                val referenceStore = Store()
+                val func = referenceStore.instantiate("mod", module.module.chicoryModule).export("store")!!
+                func.apply(*input.toLongArray())
+                val chicoryMemory = referenceStore.toImportValues().memories().single()
+                val expectedBytes = chicoryMemory.memory().readBytes(0, 65536)
+
+                val result = runner.run(ksplang, input)
+                // We expect layout to be as follows (no fun table, no globals):
+                // 0 input_len mem_size mem_max_size [memory] [input]
+                val memory = result.subList(4, 4 + 65536)
+                memory.forEachIndexed { index, value ->
+                    val expected = expectedBytes[index].toUByte().toLong()
+                    value shouldBe expected
+                    value shouldBeLessThan 256L // just a sanity check really
+                }
+            }
+        }
+    }
 })
